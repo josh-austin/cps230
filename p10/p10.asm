bc14d198a2734b7d5a64abe33ed32cb29bd814bc
cthac393 <cthac393@students.bju.edu>
2014-04-23 19:18:19 -0400

    Lines are drawing
    We've done it.

--- p10/p10.asm
+++ p10/p10.asm
@@ -1,3 +1,10 @@
+; 		   Program: CpS 230 Program 10 - Drawing Lines
+; 	   Description: Program will draw 24 lines on the screen in a
+;					predetermined pattern
+; 			Author: Cameron Thacker
+; 			 Notes: 
+;    Help Received: (None)
+
 ;.model tiny
 ;.386
 ;.stack 200h
@@ -5,11 +12,11 @@
 include irvine16.inc
 
 .data
-X_1 dword 19
-Y_1 dword 0
+X_1 dword 0
+Y_1 dword 19
 
-X_2 dword 0
-Y_2 dword 479
+X_2 dword 479
+Y_2 dword 0
 
 _B dword 19
 _M dword ?
@@ -29,14 +36,14 @@ main proc
     mov ax, 0a000h      ; Set up the video segment
     mov es, ax
 
-    mov cx, 24
+ ;   mov cx, 24
     mov di, 0
 
 L1:
     call calc_coords
 		
-	add [X_1], 20
-	sub [Y_2], 20
+	add [Y_1], 20
+	sub [X_2], 20
 	
 	dec cx
 	cmp cx, 0
@@ -106,44 +113,39 @@ calc_coords proc
 ;     where Y = ( ( rise/run ) * X ) + 20
 	pusha
 	
-	; STEP 1: Calculate slope (aka "M")
-	mov edx, 0     ; clear EDX for the upcoming IDIV
-	
-	mov eax, [Y_2]
-	sub eax, [Y_1]
-	
+	mov eax, [Y_1]
 	mov ebx, [X_2]
-	sub ebx, [X_1]
 	
-	idiv ebx
-	mov [_M], eax
+	cmp eax, ebx
+;	jg JG_ONE			; If M <= 1, then X = (Y-B) / M
 	
-	
-	imul negative_one
-;	mov [_M], eax
-	
-	; STEP 2: Calculate coordinates
-	cmp [_M], 1 ; If M <= 1, then X = (Y-B) / M
-	jl JLE_ONE
-	cmp [_M], 0
-	je J_G_ONE
+	; STEP 2: Calculate coordinates	
 
+						; y = mx + b
 
-J_G_ONE:
-	imul negative_one
-	mov eax, 0
-	mov ebx, [_B]
-	mov ecx, [X_1]
+	mov eax, 0			; set x = 0 for the first point the increment after each run through loop
+	mov ecx, [X_2]		; sets the counter equal to value of the Y_1 value
 	add ecx, 1
-	mov edx, [_M]
+	mov edx, [Y_1]		; set EDX = RISE
 
-G_ONE:
+L_ONE:
 
 	push eax
-	mov [_X], eax
-	imul edx
-	add eax, ebx
-	mov [_Y], eax
+	push ecx
+	mov [_X], eax		; set _X = EAX
+	push edx
+	neg edx
+	imul edx			; EAX = (RISE)x
+
+
+	
+	mov ecx, [X_2]		; ECX = RUN
+	idiv ecx			; EAX = (RISE)x / (RUN)
+;	call dumpregs
+	pop edx
+	pop ecx			
+	add eax, [Y_1]		; EAX = mx + b
+	mov [_Y], eax		; moves EAX value into _Y slot in memory
 	
 ;	call dumpregs
 
@@ -156,34 +158,35 @@ G_ONE:
 	cmp ecx, 0
 	je END_ALL
 
-jmp G_ONE
-
-JLE_ONE:
-	imul negative_one
+jmp L_ONE
+COMMENT @
+JG_ONE:					; x = (y-b)/m
 	pusha
 	
-	mov ecx, [Y_2]
-	add ecx, 1
-	mov eax, 0
-	mov ebx, [_B]
+	mov ecx, [X_2]		; set counter equal to Y_2 value to determine the y-length of the line
+	add ecx, 1			
+	mov eax, 0			; start from y = 0
+	mov ebx, [_B]		; move "b" value into EBX register
 	
 loopy:
 	push eax
-	mov edx, 0
+	mov edx, 0			; set edx equal to 0 for division
 	push ecx
-	mov [_Y], eax
-	mov ecx, [_M]
-	sub eax, ebx
-	idiv ecx
-	mov [_X], eax
-;call dumpregs	
-	call draw_xy
-	
-
+	mov [_Y], eax		; set y = 0 initially then increments after each run through the loop
+	sub eax, ebx		; (y - b)
+	mov ebx, [Y_1]		
+	imul ebx			; EAX = (y - b)(RISE)
+	mov ecx, [X_2]
+	idiv ecx			; EAX = ((y - b)(RISE)) / (RUN)
+	mov [_X], eax		; move the calculated x-value into the _X memory slot
+;	call dumpregs
+		
+	call draw_xy		; using the _X and _Y values, mark the point on the line
 	
 	pop ecx
 	pop eax
 	inc eax
+	
 	dec ecx
 	cmp ecx, 0
 	jne loopy
@@ -191,12 +194,11 @@ loopy:
 END_ALL:
 	
 	popa
-
-
-	
+@
+END_ALL:
 	popa
 	
-	add [_B], 20
+	add [_B], 20		; add 20 to B to mark the next y-intercept
 	
 	ret
 
@@ -226,4 +228,3 @@ write_color proc
 write_color endp
 
 end main
-
